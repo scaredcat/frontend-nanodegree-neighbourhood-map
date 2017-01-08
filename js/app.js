d091ee4f0a6cb88f976aeab6bcbe50d9cfd2d45c
scaredcat <yuna03@gmail.com>
2017-01-08 21:37:00 +1100

    added zomato api call to get restaurants in canberra

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
-//7250dfc0a3bf33128f44c564e09e66a3 - zomato
 
 var map;
+var ZOMATO_KEY = '7250dfc0a3bf33128f44c564e09e66a3';
 var SYMBOL_SVG = 'M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z';
+
+
+function getZomatoData() {
+	$.ajax({
+		url: 'https://developers.zomato.com/api/v2.1/search',
+		data: {
+			entity_id: 313, //canberra
+			entity_type: 'city',
+			radius: 500
+		},
+		type: 'GET',
+		datatype: 'json',
+		beforeSend: function(xhr) {xhr.setRequestHeader('user-key', ZOMATO_KEY);},
+		success: zomatoCallback
+	});
+}
+
+function zomatoCallback(results) {
+	for(var i=0; i < results.restaurants.length; i++) {
+		console.log(results.restaurants[i]);
+		createMarker({
+			lat: parseFloat(results.restaurants[i].restaurant.location.latitude), 
+			lng: parseFloat(results.restaurants[i].restaurant.location.longitude)
+		}, 'red');
+	}
+}
+
 
 function initMap() {
 	var canberra = new google.maps.LatLng(-35.279100, 149.131758);
@@ -20,29 +47,28 @@ function initMap() {
 	service.nearbySearch(request, callback);
 
 	// types: restaurant, cafe, bakery, bar, 
+
+	getZomatoData();
 }
 
 function callback(results, status) {
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
 		for (var i = 0; i < results.length; i++) {
 			var place = results[i];
-			createMarker(results[i]);
+			createMarker(results[i].geometry.location);
 		}
 	}
 }
 
-function createMarker(place) {
-	var placeLoc = place.geometry.location;
-
+function createMarker(location, colour = 'black') {
 	var marker = new google.maps.Marker({
 		map: map,
-		position: place.geometry.location,
+		position: location,
 		icon: {
 			path: SYMBOL_SVG,
-			fillColor: 'black',
+			fillColor: colour,
 			fillOpacity: 1,
 			anchor: {x: 12, y: 20}
-
 		}
 	});
 
@@ -57,5 +83,3 @@ function createMarker(place) {
 	// 	infowindow.open(map, this);
 	// });
 }
-
-//http://map-icons.com/

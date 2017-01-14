7777cabba295d30a99ca991526fb7ac20a6c9757
scaredcat <yuna03@gmail.com>
2017-01-14 22:05:33 +1100

    added search feature

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 var map;
 var ZOMATO_KEY = '7250dfc0a3bf33128f44c564e09e66a3';
 var SYMBOL_SVG = 'M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z';
+var CANBERRA = null;
 
+// stores the data received from the api call as well as state data
+// for the view
 var model = {
 	zomatoList: [],
 	googleList: [],
-	sidebar: {
+	sidebar: { // stores whether or not the sidebar is visible
 		visible: 0,
 		value: '>'
 	}
@@ -24,13 +27,19 @@ var sideBar = function() {
 	}, self);
 }
 
-var zomatoRestaurant = function(data) {
-	this.url = ko.observable(data.restaurant.url);
-	this.name = ko.observable(data.restaurant.name);
-	this.rating = ko.observable(data.restaurant.user_rating.aggregate_rating);
-	this.thumb = ko.observable(data.restaurant.thumb);
-	this.latitude = ko.observable(data.restaurant.location.latitude);
-	this.longitude = ko.observable(data.restaurant.location.longitude);
+/**
+* This returns an Object type of Restaurant which is then later used to filter
+* the restaurants displayed on the page
+*/
+var Restaurant = function(data) {
+	this.url = ko.observable(data.url);
+	this.name = ko.observable(data.name);
+	this.rating = ko.observable(data.rating);
+	this.thumb = ko.observable(data.thumb);
+	this.latitude = ko.observable(data.latitude);
+	this.longitude = ko.observable(data.longitude);
+	this.marker = createMarker({lat: parseFloat(data.latitude), lng: parseFloat(data.longitude)}, data.name, data.colour);
+	this.colour = data.colour;
 }
 
 var viewModel = function() {
@@ -41,11 +50,13 @@ var viewModel = function() {
 
 	this.sidebar = ko.observable(new sideBar());
 
+	this.filter = ko.observable('');
+
 	this.test = function() {
 		//console.log('it works');
 	}
 
-	this.zomatoRestaurants = ko.observableArray([]);
+	this.restaurants = ko.observableArray([]);
 
 	//zomato data
 	$.ajax({
@@ -62,58 +73,107 @@ var viewModel = function() {
 		function(results) {
 			model.zomatoList = results.restaurants;
 			for(var i=0; i < results.restaurants.length; i++) {
-				self.zomatoRestaurants.push(new zomatoRestaurant(results.restaurants[i]));
+				var restaurant = results.restaurants[i].restaurant;
+
+				self.restaurants.push(new Restaurant({
+					url: restaurant.url,
+					name: restaurant.name,
+					rating: restaurant.user_rating.aggregate_rating,
+					thumb: restaurant.thumb,
+					latitude: restaurant.location.latitude,
+					longitude: restaurant.location.longitude,
+					colour: 'red'
+				}));
 			}
-			self.markers(results);
 		}
 	);
 
+	// load Google maps place data
+	var request = {
+		location: CANBERRA,
+		radius: '500',
+		type: 'restaurant' // types: restaurant, cafe, bakery, bar, 
+	};
+
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(request, function(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			model.googleList = results;
+
+			for (var i = 0; i < results.length; i++) {
+				var place = results[i];
+				self.restaurants.push(new Restaurant({
+					url: null,
+					name: place.name,
+					rating: place.rating,
+					thumb: (place.hasOwnProperty('photos'))? place.photos[0].getUrl : '',
+					latitude: place.geometry.location.lat(),
+					longitude: place.geometry.location.lng(),
+					colour: 'black'
+				}));
+				//self.createMarker((self.restaurants())[self.restaurants().length()-1], 'black');
+				//createMarker(place.geometry.location, place);
+			}
+		}
+	});
+
+	// functions to control the view
+	this.showAllRestaurants = function() {
+		self.restaurants().forEach(function(restaurant) {
+			if(restaurant.marker) {
+				restaurant.marker.setMap(map);
+			}
+		});
+	}
+
+	this.filteredZomato = ko.computed(function() {
+	    var filter = this.filter().toLowerCase();
+	    if (!filter) {
+	    	self.showAllRestaurants();
+	        return this.restaurants();
+	    } else {
+	        return ko.utils.arrayFilter(this.restaurants(), function(restaurant) {
+	        	var show = restaurant.name().toLowerCase().substring(0, filter.length) === filter;
+	        	if (show) {
+	        		restaurant.marker.setMap(map);
+	        	} else {
+	        		restaurant.marker.setMap(null);
+	        	}
+	            return show;
+	        });
+	    }
+	}, self);
+
 	this.markers = function(results) {
-		this.zomatoRestaurants().forEach(function(item) {
-			createMarker({
+		this.restaurants().forEach(function(item) {
+			item.marker = createMarker({
 				lat: parseFloat(item.latitude()), 
 				lng: parseFloat(item.longitude())
-			}, {name: item.name()}, 'red');
+			}, {name: item.name()}, item.colour);
 		});
+	};
+
+	this.createMarker = function(restaurant, colour) {
+		restaurant.marker = createMarker({
+				lat: parseFloat(restaurant.latitude()), 
+				lng: parseFloat(restaurant.longitude())
+		}, {name: restaurant.name()}, colour);
 	}
 };
 
 
 function initMap() {
-	var canberra = new google.maps.LatLng(-35.279100, 149.131758);
+	CANBERRA = new google.maps.LatLng(-35.279100, 149.131758);
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: canberra,
+		center: CANBERRA,
 		zoom: 16,
 		mapTypeControl: false
 	});
 
-	var request = {
-		location: canberra,
-		radius: '500',
-		type: 'restaurant'
-	};
-
-	service = new google.maps.places.PlacesService(map);
-	service.nearbySearch(request, callback);
-
-	// types: restaurant, cafe, bakery, bar, 
-
 	ko.applyBindings(new viewModel());
-
-	//console.log(viewModel);
 }
 
-function callback(results, status) {
-	if (status == google.maps.places.PlacesServiceStatus.OK) {
-		model.googleList = results;
-		for (var i = 0; i < results.length; i++) {
-			var place = results[i];
-			createMarker(results[i].geometry.location, place);
-		}
-	}
-}
-
-function createMarker(location, place, colour = 'black') {
+function createMarker(location, name, colour = 'black') {
 	var marker = new google.maps.Marker({
 		map: map,
 		position: location,
@@ -130,7 +190,9 @@ function createMarker(location, place, colour = 'black') {
     });
 
 	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
+		infowindow.setContent(name);
 		infowindow.open(map, this);
 	});
+
+	return marker;
 }
